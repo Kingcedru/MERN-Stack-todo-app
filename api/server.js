a95ec96b70360107cdb6107d2e6d4afbbdeb25c6
Kingcedru <byishimocedrick@gmail.com>
2024-01-19 00:06:19 +0200

    setup

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mern-todo", {
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to DB"))
+  .catch(console.error);
+
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+app.listen(3001, () => console.log("server started on port 3001"));
