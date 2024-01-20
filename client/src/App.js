b6916cd67c66a29d494f0c1ef6aec7a8e7fedbc5
Kingcedru <byishimocedrick@gmail.com>
2024-01-20 21:46:23 +0200

    adding react

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Wolcome, King</h1>
+      <h4>Your Tasks</h4>
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">Get the bread</div>
+          <div className="delete-todo">X</div>
+        </div>
+        <div className="todo is-complete">
+          <div className="checkbox"></div>
+          <div className="text">Get the milk</div>
+          <div className="delete-todo">X</div>
+        </div>
+      </div>
     </div>
   );
 }
